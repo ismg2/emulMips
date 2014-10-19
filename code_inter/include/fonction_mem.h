4c2ebf2afd807626210a2d6b70b6d864f49935e0
Ism_g <ismail.guedira@gmail.com>
2014-10-19 23:22:24 +0200

    Fonction de lecture d'un mot de la memoire, non finie et ne fonctionnant pas

---
+++ code_inter/include/fonction_mem.h
@@ -0,0 +1,11 @@
+#ifndef _FONCTION_MEM_H
+#define _FONCTION_MEM_H
+
+#include "fonction_base.h"
+
+
+void affiche_mot (mem virtualm, uint32_t vaddr1);
+//byte * renvoi_mot (mem virtualm, int vaddr1) ;
+
+
+#endif
